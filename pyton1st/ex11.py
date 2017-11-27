45cf2d7b99fbdced6c195346f64c1777fea0d2bd
Octopusul <tavi.gavrila@gmail.com>
2017-11-27 16:42:42 +0200

    Both on same branch

---
+++ pyton1st/ex11.py
@@ -0,0 +1,9 @@
+print  "Cati ani ai?",
+age = input()
+print "Ce inaltime ai? (in cm)",
+height = raw_input()
+print "Cate kile de iarba ai?",
+weed = raw_input()
+
+print "Varsta ta este de %d, ai o inaltime de %r cm si ai %r kile de weed" % (age, height, weed)
+
---
+++ pyton1st/ex9.py
@@ -0,0 +1,10 @@
+zile = "Luni Marti Miercuri Joi Vineri"
+luni = "Ian\nFeb\nMar\nApr\nMai\nIun\nIul\nAug\nSep\nOct"
+
+print "Zilele sunt astea:", zile
+print "Iar lunile:", luni
+print """
+3 Ghilimele, wtf?
+Dude, what's wrong with u?
+Doamne frate..
+"""
